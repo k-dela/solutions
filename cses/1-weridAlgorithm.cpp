ce1207cd9175b706f5b417e33dd3a0a281de3ca5
k-dela <91074612+k-dela@users.noreply.github.com>
2021-09-21 15:23:36 -0500

    Create 1-weridAlgorithm.cpp

---
+++ cses/1-weridAlgorithm.cpp
@@ -0,0 +1,18 @@
+// https://cses.fi/problemset/task/1068/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  long n = 0;
+  cin >> n;
+  cout << n << " ";
+  while(n != 1){
+    if(n % 2 == 0){
+      n = n / 2;
+    }else{
+      n = (n * 3) + 1;
+    }
+    cout << n << " ";
+  }
+}
