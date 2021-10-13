0ef63a837274eefdb498781d883bdbafbf9ab93d
k-dela <91074612+k-dela@users.noreply.github.com>
2021-10-13 13:22:52 -0500

    Create beatiful_matrix.cpp

---
+++ codeforces/beatiful_matrix.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/contest/263/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int number;
+
+  for(int i = 1; i <= 5; i++){
+    for(int j = 1; j <= 5; j++){
+      cin >> number;
+      if(number == 1){
+        cout << abs(i - 3) + abs(j - 3) << endl;
+      }
+    }
+  }
+  
+}
+
