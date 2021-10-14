dce64cd7da29adfbd3237ebc6542e66a67756b4c
k-dela <91074612+k-dela@users.noreply.github.com>
2021-10-14 16:48:58 -0500

    Create word-capitalization.cpp

---
+++ codeforces/word-capitalization.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/contest/281/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string word;
+  cin >> word;
+
+  word[0] = toupper(word[0]);
+
+  cout << word << endl;
+}
