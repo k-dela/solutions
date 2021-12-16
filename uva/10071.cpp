2c8d9cbee7f4b0c20d9fc20600b3a432e7cd15d0
k-dela <91074612+k-dela@users.noreply.github.com>
2021-12-16 13:36:30 -0600

    Create 10071.cpp

---
+++ uva/10071.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int v,t;
+  int result;
+
+  while(cin >> v >> t){
+    result = (v*2) * t;
+    cout << result << endl;
+  }
+} 
