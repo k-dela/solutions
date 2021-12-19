8678e11b6922f86b30419bd3291f368cc0da9d30
k-dela <91074612+k-dela@users.noreply.github.com>
2021-12-19 09:14:53 -0600

    Create the_last_problem.cpp

---
+++ kattis/the_last_problem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  string name;
+  getline(cin, name);
+
+  cout << "Thank you, " << name << ", and farewell!"<< endl;
+} 
