a89b7e7bc1a772efcec3f8b81f26aa277a99c28b
k-dela <91074612+k-dela@users.noreply.github.com>
2021-12-19 08:46:37 -0600

    Create r2.cpp

---
+++ kattis/r2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int numOne, median;
+  cin >> numOne >> median;
+
+  int numTwo = (median * 2) - numOne;
+  cout << numTwo << endl;
+} 
