3ea6ce8d530ff2281f57ef59eeca4cc175da3ac4
k-dela <91074612+k-dela@users.noreply.github.com>
2022-01-27 10:30:03 -0600

    not the cleanest

---
+++ hackerRank/timeConversion.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    let hour = Number(s.substring(0,2));
+    const letters = s.substring(8);
+    
+    if(letters === 'PM'){
+        if(hour != 12){
+            hour = 12 + hour;
+        }    
+        if(hour < 10){
+            hour = `0${hour}`;
+        }
+        return hour + s.substring(2,8);
+    }else{
+        if(hour === 12){
+            hour = 0;
+        }
+        if(hour < 10){
+            hour = `0${hour}`;
+        }
+        return hour + s.substring(2,8);
+    }
+    
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
