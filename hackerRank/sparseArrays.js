d9d0ccf54ad2fd314049e98970c9d5bd235bf0a9
k-dela <91074612+k-dela@users.noreply.github.com>
2022-03-12 11:20:30 -0600

    Create sparseArrays.js

---
+++ hackerRank/sparseArrays.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY strings
+ *  2. STRING_ARRAY queries
+ */
+
+function matchingStrings(strings, queries) {
+    const result = [];
+    let counterOfCurrentQuery;
+    for(const query of queries){
+        counterOfCurrentQuery = 0;
+        for(const string of strings){
+            if(query === string){
+                counterOfCurrentQuery += 1;
+            }
+        }
+        
+        result.push(counterOfCurrentQuery);
+    }
+    
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const stringsCount = parseInt(readLine().trim(), 10);
+
+    let strings = [];
+
+    for (let i = 0; i < stringsCount; i++) {
+        const stringsItem = readLine();
+        strings.push(stringsItem);
+    }
+
+    const queriesCount = parseInt(readLine().trim(), 10);
+
+    let queries = [];
+
+    for (let i = 0; i < queriesCount; i++) {
+        const queriesItem = readLine();
+        queries.push(queriesItem);
+    }
+
+    const res = matchingStrings(strings, queries);
+
+    ws.write(res.join('\n') + '\n');
+
+    ws.end();
+}
