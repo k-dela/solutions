4ce1fc3ddcca673dcfce19a1d297d78b4ab1a7d4
k-dela <91074612+k-dela@users.noreply.github.com>
2022-03-14 13:53:18 -0600

    Create gradingStudents.js

---
+++ hackerRank/interviewPrep/week2/gradingStudents.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+  const finalGrades = [];
+  for(const grade of grades){
+    if(grade < 38){
+      finalGrades.push(grade);
+    }else{
+      if((grade + 1) % 5 === 0){
+        finalGrades.push(grade+1);
+      }else if((grade +2) % 5 === 0){
+        finalGrades.push(grade+2);
+      }else{
+        finalGrades.push(grade);
+      }
+    }
+  }
+  
+  return finalGrades;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const gradesCount = parseInt(readLine().trim(), 10);
+
+    let grades = [];
+
+    for (let i = 0; i < gradesCount; i++) {
+        const gradesItem = parseInt(readLine().trim(), 10);
+        grades.push(gradesItem);
+    }
+
+    const result = gradingStudents(grades);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
