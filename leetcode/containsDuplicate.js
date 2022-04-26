a1a0d4df4b1f8592f912db22a942643754baadc1
k-dela <91074612+k-dela@users.noreply.github.com>
2022-04-26 10:10:36 -0500

    I like this solution more

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = 0; j < nums.length; j++){
-            if(j != i){
-                if(nums[i] == nums[j]){
-                    return true;
-                }
-            }
+    const trackerNums = {};
+    
+    for(let num of nums){
+        if(trackerNums[num]){
+            return true;
+        }else{
+            trackerNums[num] = true;
         }
     }
+    
     
     return false;
 };
