b69290c6606728a78365875a894f9a1eb34c81cc
k-dela <91074612+k-dela@users.noreply.github.com>
2022-06-17 15:59:40 -0500

    exploring

---
+++ leetcode/CPP/maximunSubArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int maxSum = nums[0];
+      int currentSum = 0;
+      
+      for(int i  = 0; i < nums.size(); i++){
+        if(currentSum < 0){
+          currentSum = 0;
+        }
+        
+        currentSum += nums[i];
+        
+        if(currentSum > maxSum){
+          maxSum = currentSum;
+        }
+      }
+      
+      return maxSum;
+      
+    }
+};
