68062f7b5fed793e86f78ae6f5e1e2a142a76858
k-dela <91074612+k-dela@users.noreply.github.com>
2022-07-02 12:03:12 -0500

    great!

---
+++ hackerRank/Data Structures/insertNodeAtTheTailOfALinkedList.cpp
@@ -0,0 +1,31 @@
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtTail(SinglyLinkedListNode* head, int data) {
+	SinglyLinkedListNode* newNode = new SinglyLinkedListNode(data);
+    
+    if(head == NULL){
+        head = newNode;
+        return head;
+    }
+    
+    
+    SinglyLinkedListNode* temp = head;
+    
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    
+    
+    temp-> next = newNode;
+    
+    return head;
+}
